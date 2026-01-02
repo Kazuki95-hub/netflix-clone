a24d1595150d4399ad43aa7acaeb659303489bad
Kazuki95-hub <78989081+Kazuki95-hub@users.noreply.github.com>
2026-01-03 00:32:58 +0900

    素のaxiosではなく自作axiosインスタンスを使用するよう修正し、レスポンスで返ってきた値を見れるようにした

--- netflix-clone/src/App.tsx
+++ netflix-clone/src/App.tsx
@@ -2,6 +2,7 @@ import { requests } from "./request"
 import { Row } from "./components/Row";
 
 function App() {
+  console.log("App rendered");
   return (
     <div className="App">
       <Row fetchUrl={requests.fetchNetflixOriginals} />
